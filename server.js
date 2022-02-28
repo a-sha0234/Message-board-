7035b4f4c7da5d2fda7a4e3ad153c66aa2920b80
a-sha0234 <asharma_31@hotmail.com>
2022-02-28 23:28:13 +0000

    add navbar

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 app.set("view engine", "ejs");
-
+app.use(express.urlencoded({ extended: true }));
 const messages = [
   {
     text: "Hi there!",
@@ -21,7 +21,16 @@ app.get("/", (req, res) => {
 });
 
 app.get("/new", (req, res) => {
-  res.render("new");
+  res.render("new", { title: "Add message" });
+});
+
+app.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.messageUser,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 app.listen(3000);
