287d53eb13ac84d4e8372d0f73f098f926c2e9a5
a-sha0234 <asharma_31@hotmail.com>
2022-03-01 16:49:36 +0000

    add basic styling

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 const messages = [
   {
     text: "Hi there!",
